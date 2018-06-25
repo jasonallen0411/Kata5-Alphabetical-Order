197534d72c70bd4e58656c2cd84302c8156b2a2f
Jason Allen <jasonallen@Jasons-MacBook-Pro.local>
2018-06-25 08:16:15 -0500

    Initial Commit

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+$("#kataFive").click(function(){
+	var alphaArray = ["Green", "Black", "White", "Red", "Amy", "Zoo", "Salmon"];
+	alphaArray.sort();
+	alert(alphaArray);
+	$("#kataFiveArea").html(alphaArray);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
